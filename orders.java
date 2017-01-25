e0c93fc850ef03d66127ea77ac2baa124627af39
halweh99 <halweh1707@volmail.jalc.edu>
2017-01-25 11:43:23 -0600

    Add files via upload

---
+++ orders.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class orders {
+       
+   public static void main(String[] args) throws Exception {
+   Scanner keyboard=new Scanner(System.in);
+    
+      //Declaring variables
+        
+      String name;
+      double price;
+      int quantity;
+     
+     for(String new filename; args){throws Exception
+     new(readfile(filename);
+     }
+     catch (IOException x) {
+     System.out.println ("PLease insert filename");
+     filename= System.keyboard.nextLine(); 
+       
+      String: filename= pullfilename (new filename)
+      Scanner input= new scanner(file);
+      
+       
+     }
+         
+    }
+     
+     
+        
+   }
+ }
+  
